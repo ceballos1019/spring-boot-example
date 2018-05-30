bda8d6b8afa1aa958bc4630d78d6bd7c303fc5a3
Andres Ceballos Sanchez <andres.ceballos@ibm.com>
2018-05-30 11:42:19 -0500

    Added two views to the example project

---
+++ src/main/java/com/ibm/springboot/tutorial/HomeController.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.ibm.springboot.tutorial;
+
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author andressanchez
+ *
+ */
+@RestController
+public class HomeController {
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello!";
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public ModelAndView save(@ModelAttribute User user) {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("user-data");
+		modelAndView.addObject("user", user);
+		return modelAndView;
+	}
+
+}
---
+++ src/main/java/com/ibm/springboot/tutorial/SpringBootExampleApplication.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.ibm.springboot.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author andressanchez
+ *
+ */
+@SpringBootApplication
+public class SpringBootExampleApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExampleApplication.class, args);
+	}
+
+}
